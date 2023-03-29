e1bb8270504ad67d3c8d12205dd1432e0cf17fb2
syedazlan833 <120971357+syedazlan833@users.noreply.github.com>
2023-03-29 16:18:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,295 @@
+// var azlan = prompt("What Pizza Would You Like To Eat. ", "Large Or Small")
+// if (azlan === "Large"||"Large "||"large"||"large " || "Small" || "small"||"Small "){
+//     var nprom = prompt("Chicken , Beef , Or Veg")
+//     if(nprom === "Chicken" ||"chicken"||"Chicken " || "Beef" || "Beef " || "beef" || "Veg" || "Veg " || "veg" || "Vegetable"){
+//       var neprom = prompt("SIR.Its 1000Rs How Much Pizza You Take")
+//        var multiplication = neprom*1000
+//        alert(multiplication + "Rs And in 15 minutes Pizza Iz Delivered in Your Home")
+//     }
+// }
+
+
+// var spaces = prompt("Give Only Double Spaces")
+
+// if (spaces === "  ") {
+//     alert("ok!")
+// } else (
+//     alert("Give Only Double Spaces!!")
+// )
+
+// var Sent = prompt("enter your name");
+// var words = Sent.split(" ");
+// for (var i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i];
+// }
+// words.join(" ");
+// document.write(words)
+ 
+var team = prompt("HELLO SIR, How would you like To decide the players in a team ")
+for (i = 1; i< 5+1; i++){
+    if(team === "5"){
+      var pp =  prompt("Palyer" + [i])
+    }
+}
+for (ia = 1; ia< 4+1; ia++){
+    if(team === "4"){
+      var pp =  prompt("Palyer" + [ia])
+    }
+}
+for (ib = 1; ib< 3+1; ib++){
+    if(team === "3"){
+      var pp =  prompt("Palyer" + [ib])
+    }
+}
+for (ic = 1; ic< 2+1; ic++){
+    if(team === "2"){
+      var pp =  prompt("Palyer" + [ic])
+    }
+}
+for (id = 1; id< 1+1; id++){
+    if(team === "1"){
+      var pp =  prompt("Palyer" + [id])
+    }
+}
+for (a = 1; a< 10+1; a++){
+    if(team === "10"){
+      var pp =  prompt("Palyer" + [a])
+    }
+}
+for (b = 1; b< 20+1; b++){
+    if(team === "20"){
+      var pp =  prompt("Palyer" + [b])
+    }
+}
+for (c = 1; c< 50+1; c++){
+    if(team === "50"){
+      var pp =  prompt("Palyer" + [c])
+    }
+}
+for (d = 1; d< 6+1; d++){
+    if(team === "6"){
+      var pp =  prompt("Palyer" + [d])
+    }
+}
+for (e = 1; e< 7+1; e++){
+    if(team === "7"){
+      var pp =  prompt("Palyer" + [e])
+    }
+}
+for (f = 1; f< 8+1; f++){
+    if(team === "8"){
+      var pp =  prompt("Palyer" + [f])
+    }
+}
+for (g = 1; g< 9+1; g++){
+    if(team === "9"){
+      var pp =  prompt("Palyer" + [g])
+    }
+}
+for (h = 1; h< 10+1; h++){
+    if(team === "10"){
+      var pp =  prompt("Palyer" + [h])
+    }
+}
+for (j = 1; j< 11+1; j++){
+  if(team === "11"){
+    var pp =  prompt("Palyer" + [j])
+  }
+}
+for (k = 1; k< 12+1; k++){
+  if(team === "12"){
+    var pp =  prompt("Palyer" + [k])
+  }
+}
+for (l = 1; l< 13+1; l++){
+  if(team === "13"){
+    var pp =  prompt("Palyer" + [l])
+  }
+}
+for (m = 1; m< 14+1; m++){
+  if(team === "14"){
+    var pp =  prompt("Palyer" + [m])
+  }
+}
+for (n = 1; n< 16+1; n++){
+  if(team === "16"){
+    var pp =  prompt("Palyer" + [n])
+  }
+}
+for (o = 1; o< 17+1; o++){
+  if(team === "17"){
+    var pp =  prompt("Palyer" + [o])
+  }
+}
+for (p = 1; p< 18+1; p++){
+  if(team === "18"){
+    var pp =  prompt("Palyer" + [p])
+  }
+}
+for (q = 1; q< 19+1; q++){
+  if(team === "19"){
+    var pp =  prompt("Palyer" + [q])
+  }
+}
+for (r = 1; r< 21+1; r++){
+  if(team === "21"){
+    var pp =  prompt("Palyer" + [r])
+  }
+}
+for (s = 1; s< 22+1; s++){
+  if(team === "22"){
+    var pp =  prompt("Palyer" + [s])
+  }
+}
+for (t = 1; t< 23+1; t++){
+  if(team === "23"){
+    var pp =  prompt("Palyer" + [t])
+  }
+}
+for (u = 1; u< 24+1; u++){
+  if(team === "24"){
+    var pp =  prompt("Palyer" + [u])
+  }
+}
+for (v = 1; v< 25+1; v++){
+  if(team === "25"){
+    var pp =  prompt("Palyer" + [v])
+  }
+}
+for (w = 1; w< 26+1; w++){
+  if(team === "26"){
+    var pp =  prompt("Palyer" + [w])
+  }
+}
+for (x = 1; x< 27+1; x++){
+  if(team === "27"){
+    var pp =  prompt("Palyer" + [x])
+  }
+}
+for (y = 1; y< 28+1; y++){
+  if(team === "28"){
+    var pp =  prompt("Palyer" + [y])
+  }
+}
+for (z = 1; z< 29+1; z++){
+  if(team === "29"){
+    var pp =  prompt("Palyer" + [z])
+  }
+}
+for (ab = 1; ab< 30+1; ab++){
+  if(team === "30"){
+    var pp =  prompt("Palyer" + [ab])
+  }
+}
+for (ac = 1; ac< 31+1; ac++){
+  if(team === "31"){
+    var pp =  prompt("Palyer" + [ac])
+  }
+}
+for (ad = 1; ad< 32+1; ad++){
+  if(team === "32"){
+    var pp =  prompt("Palyer" + [ad])
+  }
+}
+for (ae = 1; ae< 33+1; ae++){
+  if(team === "33"){
+    var pp =  prompt("Palyer" + [ae])
+  }
+}
+for (af = 1; af< 34+1; af++){
+  if(team === "34"){
+    var pp =  prompt("Palyer" + [af])
+  }
+}
+for (ag = 1; ag< 35+1; ag++){
+  if(team === "35"){
+    var pp =  prompt("Palyer" + [ag])
+  }
+}
+for (ah = 1; ah< 36+1; ah++){
+  if(team === "36"){
+    var pp =  prompt("Palyer" + [ah])
+  }
+}
+for (ai = 1; ai< 37+1; ai++){
+  if(team === "37"){
+    var pp =  prompt("Palyer" + [ai])
+  }
+}
+for (aj = 1; aj< 38+1; aj++){
+  if(team === "38"){
+    var pp =  prompt("Palyer" + [aj])
+  }
+}
+for (ak = 1; ak< 39+1; ak++){
+  if(team === "39"){
+    var pp =  prompt("Palyer" + [ak])
+  }
+}
+for (al = 1; al< 40+1; al++){
+  if(team === "40"){
+    var pp =  prompt("Palyer" + [al])
+  }
+}
+for (am = 1; am< 41+1; am++){
+  if(team === "41"){
+    var pp =  prompt("Palyer" + [am])
+  }
+}
+for (an = 1; an< 42+1; an++){
+  if(team === "42"){
+    var pp =  prompt("Palyer" + [an])
+  }
+}
+for (ao = 1; ao< 43+1; ao++){
+  if(team === "43"){
+    var pp =  prompt("Palyer" + [ao])
+  }
+}
+for (ap = 1; ap< 44+1; ap++){
+  if(team === "44"){
+    var pp =  prompt("Palyer" + [ap])
+  }
+}
+for (aq = 1; aq< 45+1; aq++){
+  if(team === "45"){
+    var pp =  prompt("Palyer" + [aq])
+  }
+}
+for (ar = 1; ar< 46+1; ar++){
+  if(team === "46"){
+    var pp =  prompt("Palyer" + [ar])
+  }
+}
+for (at = 1; at< 47+1; at++){
+  if(team === "47"){
+    var pp =  prompt("Palyer" + [at])
+  }
+}
+for (av = 1; av< 49+1; av++){
+  if(team === "49"){
+    var pp =  prompt("Palyer" + [av])
+  }
+}
+for (ax = 1; ax< 48+1; ax++){
+  if(team === "48"){
+    var pp =  prompt("Palyer" + [ax])
+  }
+}
+for (aw = 1; aw< 50+1; aw++){
+  if(team === "50"){
+    var pp =  prompt("Palyer" + [aw])
+    break
+  }
+}
+if(team >= "51"){
+  alert("The Team Will Not Go Further! Because 50 Is Last")
+}
+if(team === ""){
+  alert("Don't give the input empty")
+}
+if(team === " " || team === "  " ||team === "  "){
+  alert("Don't give spaces")
+}
+
+
